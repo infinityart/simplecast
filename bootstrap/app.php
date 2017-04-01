ff6da821316eba0e1387bad66df128a8578bb48d
Donny van Walsem <donnehvw@gmail.com>
2017-04-01 17:25:26 +0200

    Add flip\whoops and dotenv and simple environmentswitcher

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,4 +1,19 @@
 <?php
 require "../vendor/autoload.php";
 
+$dotenv = new Dotenv\Dotenv('../');
+$dotenv->load();
+
+$whoops = new Whoops\Run();
+if (getenv('ENVIRONMENT') !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e){
+        echo 'Todo: Friendly error page and send an email to the developer';
+    });
+}
+$whoops->register();
+
 require "../app/routes.php";
+
+throw new Exception();
